4247d7d4a25c571c9c5a8d7767b24187202979c5
Li Jinbo <pozi119@163.com>
2019-10-11 17:55:25 +0800

    Cacheable & MMapKVCache

--- Storage/Classes/Storage.swift
+++ Storage/Classes/Storage.swift
@@ -4,10 +4,10 @@ import SwiftSignalKitMac
 
 protocol Storable: Codable, Equatable {}
 
-public protocol Table {
-    func begin()
-    func commit()
-    func rollback()
-
-    func transaction<T>(_ block: () -> Void) -> Signal<T, Error>
-}
+//public protocol Table {
+//    func begin()
+//    func commit()
+//    func rollback()
+//
+//    func transaction<T>(_ block: () -> Void) -> Signal<T, Error>
+//}
---
+++ Storage/Classes/Tables/Table.swift
@@ -0,0 +1,11 @@
+//
+//  Table.swift
+//  Storage
+//
+//  Created by Valo on 2019/10/11.
+//
+
+import Foundation
+
+open class Table {
+}
