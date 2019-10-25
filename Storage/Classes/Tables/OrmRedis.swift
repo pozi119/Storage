bd4db30a678e0fd7685c4803cd14b4cc46826beb
Li Jinbo <pozi119@163.com>
2019-10-25 11:03:52 +0800

    optimize OrmRedis

--- Storage/Classes/Tables/OrmRedis.swift
+++ Storage/Classes/Tables/OrmRedis.swift
@@ -16,38 +16,37 @@ extension Orm: Redisable where T: Equatable {
     public typealias Value = T
 
     public func primaryKeyValue(of key: String) -> [String: Binding]? {
-        guard let conf = config as? PlainConfig else { return nil }
-        guard conf.primaries.count > 0 else { return nil }
+        guard config.primaries.count > 0 else { return nil }
         let values = key.components(separatedBy: separator)
-        guard values.count == conf.primaries.count else { return nil }
+        guard values.count == config.primaries.count else { return nil }
         var result: [String: Binding] = [:]
         for i in 0 ..< values.count {
-            let k = conf.primaries[i]
+            let k = config.primaries[i]
             let v = values[i]
             result[k] = v
         }
         return result
     }
 
-    public func keyValue(of value: T) -> (String, [String: Binding]) {
+    public func keyValue(of value: Value) -> (String, [String: Binding]) {
         let result: (String, [String: Binding]) = ("", [:])
-        guard let conf = config as? PlainConfig,
-            conf.primaries.count > 0,
+        guard config.primaries.count > 0,
             let _val = try? encoder.encode(value) as? [String: Binding] else { return result }
-        let array = conf.primaries.map { String(describing: _val[$0] ?? "") }
+        let array = config.primaries.map { String(describing: _val[$0] ?? "") }
         let _key = array.joined(separator: separator)
         return (_key, _val)
     }
 
-    private func constraint(lower: String?, upper: String?, bounds: Bounds, primaries: [String]) -> Where? {
-        let count = primaries.count
+    private func constraint(lower: String?, upper: String?, bounds: Bounds) -> Where? {
+        guard config.primaries.count > 0 else { return nil }
+        let count = config.primaries.count
         var lvalues = (lower ?? "").components(separatedBy: separator)
         var uvalues = (upper ?? "").components(separatedBy: separator)
         lvalues = lvalues + Array(repeating: "", count: Swift.max(0, count - lvalues.count))
         uvalues = uvalues + Array(repeating: "", count: Swift.max(0, count - uvalues.count))
         var condition = Where("")
         for i in 0 ..< count {
-            let pk = primaries[i], l = lvalues[i], u = uvalues[i]
+            let pk = config.primaries[i], l = lvalues[i], u = uvalues[i]
             condition = condition && (bounds.contains(.lower) ? (Where(pk) >= l) : (Where(pk) > l))
             if u.count == 0 { continue }
             condition = condition && (bounds.contains(.upper) ? (Where(pk) <= u) : (Where(pk) < u))
@@ -55,7 +54,7 @@ extension Orm: Redisable where T: Equatable {
         return condition
     }
 
-    public func set(_ key: String, value: T) -> Int {
+    public func set(_ key: String, value: Value) -> Int {
         if let oldValue = get(key), oldValue == value {
             return 0
         }
@@ -107,12 +106,10 @@ extension Orm: Redisable where T: Equatable {
                      limit: Int? = nil,
                      bounds: Bounds,
                      order desc: Bool = false) -> [String] {
-        guard let conf = config as? PlainConfig,
-            conf.primaries.count > 0,
-            let condition = constraint(lower: lower, upper: upper, bounds: bounds, primaries: conf.primaries)
-        else { return [] }
-        let orderBy = OrderBy(desc ? conf.primaries.map { $0 + " DESC" } : conf.primaries)
-        let items = xFind(condition, fields: Fields(conf.primaries), orderBy: orderBy, limit: Int64(limit ?? 0))
+        guard config.primaries.count > 0,
+            let condition = constraint(lower: lower, upper: upper, bounds: bounds) else { return [] }
+        let orderBy = OrderBy(desc ? config.primaries.map { $0 + " DESC" } : config.primaries)
+        let items = xFind(condition, fields: Fields(config.primaries), orderBy: orderBy, limit: Int64(limit ?? 0))
         return items.map { keyValue(of: $0).0 }
     }
 
@@ -121,11 +118,9 @@ extension Orm: Redisable where T: Equatable {
                      limit: Int? = nil,
                      bounds: Bounds,
                      order desc: Bool = false) -> [(String, Value)] {
-        guard let conf = config as? PlainConfig,
-            conf.primaries.count > 0,
-            let condition = constraint(lower: lower, upper: upper, bounds: bounds, primaries: conf.primaries)
-        else { return [] }
-        let orderBy = OrderBy(desc ? conf.primaries.map { $0 + " DESC" } : conf.primaries)
+        guard config.primaries.count > 0,
+            let condition = constraint(lower: lower, upper: upper, bounds: bounds) else { return [] }
+        let orderBy = OrderBy(desc ? config.primaries.map { $0 + " DESC" } : config.primaries)
         let items = xFind(condition, orderBy: orderBy, limit: Int64(limit ?? 0))
         return items.map { (keyValue(of: $0).0, $0) }
     }
